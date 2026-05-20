685711c9a3a14d2626958fc4bba59036b65ad7b8
Naisha Sarna <naisha.sarna@gmail.com>
2026-05-20 09:09:28 -0500

    Deleted initial commit to redo later

--- trading-news/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install -r requirements.txt
-echo.
-echo Starting Trading News Dashboard...
-echo Open http://localhost:8000 in your browser
-echo.
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload
